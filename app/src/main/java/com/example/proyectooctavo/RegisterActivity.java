5192d59444c9ff9012c8ff69e609539c19934190
DAYANNA LIZZET VELEZ ARAUJO <dayanna.velez2017@uteq.edu.ec>
2021-08-02 19:24:41 -0500

    Intento 1

--- app/src/main/java/com/example/proyectooctavo/RegisterActivity.java
+++ app/src/main/java/com/example/proyectooctavo/RegisterActivity.java
@@ -37,21 +37,10 @@ public class RegisterActivity extends AppCompatActivity  {
     EditText et_name,et_lastname,et_mail,et_user, et_pass,et_pass2;
     Button  btnSignUp;
 
-    AwesomeValidation awesomeValidation;
-    // para iniciar sesión con Facebook
-    FirebaseAuth firebaseAuth;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_register);
-
-        firebaseAuth = firebaseAuth.getInstance();
-        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
-        // validando el filtro email correcto
-        awesomeValidation.addValidation(this,R.id.et_mail, Patterns.EMAIL_ADDRESS,R.string.invalid_mail);
-        // validando el filtro password que sea min 6 caracteres
-        awesomeValidation.addValidation(this, R.id.et_pass,".{6,}",R.string.invalid_password );
 
 
         et_name = findViewById(R.id.et_name);
@@ -62,127 +51,9 @@ public class RegisterActivity extends AppCompatActivity  {
         et_pass2 = findViewById(R.id.et_pass2);
         btnSignUp = findViewById(R.id.btnSignUp);
 
-        btnSignUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-                String mail = et_mail.getText().toString();
-                String pass = et_pass.getText().toString();
-
-                // preguntando si todo está bien, es decir si todo los campos estan validados
-                if(awesomeValidation.validate()){
-                    // creando un usuario con un email y password
-                    firebaseAuth.createUserWithEmailAndPassword(mail,pass).
-                            addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            // si todo está correcto le decimos que
-                            if(task.isSuccessful()){
-                                Toast.makeText(RegisterActivity.this, "Usuario creado con éxito", Toast.LENGTH_SHORT).show();
-                                // eliminando la actividad que está creada
-                                finish();
-                            }else {
-                                String errorCode;
-                                if(task.getException() instanceof FirebaseAuthException){
-                                    errorCode = ((FirebaseAuthException) task.getException()).getErrorCode();
-                                    dameToastdeerror(errorCode);
-
-                                }else{
-                                    errorCode="";
-                                }
-                            }
-                        }
-                    });
-                }else{
-                    Toast.makeText(RegisterActivity.this,
-                            "Complete todos los datos por favor",
-                            Toast.LENGTH_SHORT).show();
-                }
-
-            }
-        });
       //  Bundle bundle = this.getIntent().getExtras();
 
     }
-    // posibles errores que pueden existir
-    private void dameToastdeerror(String error) {
 
-        switch (error) {
-
-            case "ERROR_INVALID_CUSTOM_TOKEN":
-                Toast.makeText(RegisterActivity.this, "El formato del token personalizado es incorrecto. Por favor revise la documentación", Toast.LENGTH_LONG).show();
-                break;
-
-            case "ERROR_CUSTOM_TOKEN_MISMATCH":
-                Toast.makeText(RegisterActivity.this, "El token personalizado corresponde a una audiencia diferente.", Toast.LENGTH_LONG).show();
-                break;
-
-            case "ERROR_INVALID_CREDENTIAL":
-                Toast.makeText(RegisterActivity.this, "La credencial de autenticación proporcionada tiene un formato incorrecto o ha caducado.", Toast.LENGTH_LONG).show();
-                break;
-
-            case "ERROR_INVALID_EMAIL":
-                Toast.makeText(RegisterActivity.this, "La dirección de correo electrónico está mal formateada.", Toast.LENGTH_LONG).show();
-                et_mail.setError("La dirección de correo electrónico está mal formateada.");
-                et_mail.requestFocus();
-                break;
-
-            case "ERROR_WRONG_PASSWORD":
-                Toast.makeText(RegisterActivity.this, "La contraseña no es válida o el usuario no tiene contraseña.", Toast.LENGTH_LONG).show();
-                et_pass.setError("la contraseña es incorrecta ");
-                et_pass.requestFocus();
-                et_pass.setText("");
-                break;
-
-            case "ERROR_USER_MISMATCH":
-                Toast.makeText(RegisterActivity.this, "Las credenciales proporcionadas no corresponden al usuario que inició sesión anteriormente..", Toast.LENGTH_LONG).show();
-                break;
-
-            case "ERROR_REQUIRES_RECENT_LOGIN":
-                Toast.makeText(RegisterActivity.this,"Esta operación es sensible y requiere autenticación reciente. Inicie sesión nuevamente antes de volver a intentar esta solicitud.", Toast.LENGTH_LONG).show();
-                break;
-
-            case "ERROR_ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL":
-                Toast.makeText(RegisterActivity.this, "Ya existe una cuenta con la misma dirección de correo electrónico pero diferentes credenciales de inicio de sesión. Inicie sesión con un proveedor asociado a esta dirección de correo electrónico.", Toast.LENGTH_LONG).show();
-                break;
-
-            case "ERROR_EMAIL_ALREADY_IN_USE":
-                Toast.makeText(RegisterActivity.this, "La dirección de correo electrónico ya está siendo utilizada por otra cuenta..   ", Toast.LENGTH_LONG).show();
-                et_mail.setError("La dirección de correo electrónico ya está siendo utilizada por otra cuenta.");
-                et_mail.requestFocus();
-                break;
-
-            case "ERROR_CREDENTIAL_ALREADY_IN_USE":
-                Toast.makeText(RegisterActivity.this, "Esta credencial ya está asociada con una cuenta de usuario diferente.", Toast.LENGTH_LONG).show();
-                break;
-
-            case "ERROR_USER_DISABLED":
-                Toast.makeText(RegisterActivity.this, "La cuenta de usuario ha sido inhabilitada por un administrador..", Toast.LENGTH_LONG).show();
-                break;
-
-            case "ERROR_USER_TOKEN_EXPIRED":
-                Toast.makeText(RegisterActivity.this, "La credencial del usuario ya no es válida. El usuario debe iniciar sesión nuevamente.", Toast.LENGTH_LONG).show();
-                break;
-
-            case "ERROR_USER_NOT_FOUND":
-                Toast.makeText(RegisterActivity.this, "No hay ningún registro de usuario que corresponda a este identificador. Es posible que se haya eliminado al usuario.", Toast.LENGTH_LONG).show();
-                break;
-
-            case "ERROR_INVALID_USER_TOKEN":
-                Toast.makeText(RegisterActivity.this, "La credencial del usuario ya no es válida. El usuario debe iniciar sesión nuevamente.", Toast.LENGTH_LONG).show();
-                break;
-
-            case "ERROR_OPERATION_NOT_ALLOWED":
-                Toast.makeText(RegisterActivity.this, "Esta operación no está permitida. Debes habilitar este servicio en la consola.", Toast.LENGTH_LONG).show();
-                break;
-
-            case "ERROR_WEAK_PASSWORD":
-                Toast.makeText(RegisterActivity.this, "La contraseña proporcionada no es válida..", Toast.LENGTH_LONG).show();
-                et_pass.setError("La contraseña no es válida, debe tener al menos 6 caracteres");
-                et_pass.requestFocus();
-                break;
-
-        }
-
-    }
 }
